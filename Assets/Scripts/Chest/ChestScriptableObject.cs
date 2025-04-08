0f7d0c0d7bb56f2e00acf4d3753c785c80e60a12
abhi6403 <163630847+abhi6403@users.noreply.github.com>
2025-04-08 15:11:14 +0530

    Created Scriptable Objects of chest

--- Assets/Scripts/Chest/ChestScriptableObject.cs
+++ Assets/Scripts/Chest/ChestScriptableObject.cs
@@ -1,7 +1,34 @@
+using System;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "ChestScriptableObject", menuName = "Scriptable Objects/ChestScriptableObject")]
-public class ChestScriptableObject : ScriptableObject
+namespace ChestSystem.Chest
 {
-    
+    [CreateAssetMenu(fileName = "Create New Chest", menuName = "Scriptable Objects/ChestScriptableObject")]
+
+    public class ChestScriptableObject : ScriptableObject
+    {
+        public Sprite _chestClosedImage;
+        public Sprite _chestOpenImage;
+        public ChestType _chestType;
+        public ChestRewards _chestRewards;
+        public float _chestTimer;
+
+        [Serializable]
+        public class ChestRewards
+        {
+            public int _minCoins;
+            public int _maxCoins;
+            public int _minGems;
+            public int _maxGems;
+        }
+
+        [Serializable]
+        public enum ChestType
+        {
+            COMMON,
+            RARE,
+            EPIC,
+            LEGENDARY
+        }
+    }
 }
